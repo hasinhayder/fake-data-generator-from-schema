598cc4599348a7eb909a445278c88cabd2d0a3c4
Hasin Hayder <countdraculla@gmail.com>
2021-05-15 07:19:04 +0600

    Student Data Schema

--- schema.mjs
+++ schema.mjs
@@ -1,9 +1,20 @@
 export default {
   output:'./output.json',
-  total:2000,
+  total:20,
   structure:{
     firstName:'firstName',
     lastName:'lastName',
-    
+    grade:'randomItem,7,11',
+    section:'Chery',
+    marks:{
+      return:'single',
+      structure:{
+        physics:'randomItem,A+,A,A-,B+,B,B-,C',
+        math:'randomItem,A+,A,A-,B+,B,B-,C',
+        chemistry:'randomItem,A+,A,A-,B+,B,B-,C',
+        geography:'randomItem,A+,A,A-,B+,B,B-,C',
+      }
+    },
+    major:'randomItemN,2,physics,chemistry,math,geography'
   }
 }
