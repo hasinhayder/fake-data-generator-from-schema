da3f6e931b3bd62a42dca20210ba276d007b6390
Hasin Hayder <hasin@leevio.com>
2022-07-16 01:21:25 +0600

    course schema

--- schema.mjs
+++ schema.mjs
@@ -1,12 +1,32 @@
 export default {
   // random 20 people with autoincrement 
   output:'./output.json',
-  total:20,
+  total:7,
   structure:{
     id:'autoIncrement',
-    firstName: 'maleFirstName',
-    lastName: 'maleLastName',
-    email:'emailFromNameAndDomain,firstName,lastName,test.com',
-    fullName:'joinerSpace,firstName,lastName',
+    cid:'uuid',
+    title: 'words,5',
+    description:'sentence',
+    episodes: {
+      total:'random',
+      limit:7,
+      structure:{
+        id:'uuid',
+        title:'words,5',
+        duration:'timeMinuteSecond',
+        bookmarked:'boolean',
+        completed:'boolean',
+        note:'unsplash,,,happy',
+        download:'unsplash,,,cute',
+        video:{
+          total:1,
+          structure:{
+            type:'randomItem,youtube,vimeo,wistia,self',
+            url:'unsplash,,,landscape'
+          }
+        }
+      }
+      // duration:'timeMinuteSecond',
+    }
   }
 }
